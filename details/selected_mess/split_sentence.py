37d7a93198b1f92db0bc9eff15d725bcfc26cd6c
reflex_zj <813577079@qq.com>
2017-08-03 11:11:36 +0800

    split_sentence

--- details/selected_mess/split_sentence.py
+++ details/selected_mess/split_sentence.py
@@ -9,7 +9,7 @@ def get_result(str ,result, tags, ids):
     id_list = []
     for x in ids:
         id = x.strip('\n').split(';')
-        id_list.append([id[0], id[1]])
+        id_list.append([id[0], id[1], id[2]])
 
     flag = 0
     for x in str:
@@ -17,20 +17,7 @@ def get_result(str ,result, tags, ids):
         id = id_list[flag]
         flag = flag+1
 
-        # 信息抽取，正则匹配信息
-        # basic = re.compile(r'；')
-        # order = re.compile('\d{1,2}\.\s*')
-        # column = re.compile(r'\s+[\u4e00-\u9fa5]+\s{0,1}：')
-
-        # 用指定的分割符来实现句子的分割
-        # x = basic.sub('。', x)
-        # x = order.sub('。', x)
-        # x = column.sub('。', x)
-        # print x
-        # setence = re.split(r'。|；|;|\s+[\u4e00-\u9fa5]+\s*:\s*|：|\s+\d{1,2}\.\s+', x)
-
         setence = x.split('。')
-
 
         for x in setence:
             # 去除多余空格
@@ -38,7 +25,7 @@ def get_result(str ,result, tags, ids):
             data = ' '.join(x.split())
             if data:
                 result.write(data+'\n')
-                tags.write(id[0] + ' ' + id[1]+ '\n')
+                tags.write(id[0] + ' ' + id[1]+ ' '+ id[2] +'\n')
             print data
 
 
